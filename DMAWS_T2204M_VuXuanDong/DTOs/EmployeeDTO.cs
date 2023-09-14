bbc803cabc8b4b5784b9eabdf8369f420d2ac8f3
Đồng Vũ <Dongvu7173@gmail.com>
2023-09-14 11:08:17 +0700

    3

--- DMAWS_T2204M_VuXuanDong/DTOs/EmployeeDTO.cs
+++ DMAWS_T2204M_VuXuanDong/DTOs/EmployeeDTO.cs
@@ -1,13 +1,25 @@
 ﻿using DMAWS_T2204M_VuXuanDong.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace DMAWS_T2204M_VuXuanDong.DTOs
 {
     public class EmployeeDTO
     {
+        [Key]
         public int EmployeeId { get; set; }
+
+        [Required]
+        [StringLength(150, MinimumLength = 2)]
         public string EmployeeName { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
         public DateTime EmployeeDOB { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string EmployeeDepartment { get; set; }
-        public virtual ICollection<Employee> Employees { get; set; }
+
+        public virtual ICollection<ProjectEmployee> ProjectEmployees { get; set; }
     }
 }
--- DMAWS_T2204M_VuXuanDong/Models/ProjectEmployee.cs
+++ DMAWS_T2204M_VuXuanDong/Models/ProjectEmployee.cs
@@ -1,9 +1,12 @@
-﻿namespace DMAWS_T2204M_VuXuanDong.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DMAWS_T2204M_VuXuanDong.Models
 {
     public class ProjectEmployee
     {
         public int EmployeeId { get; set; }
         public int ProjectId { get; set; }
+        [Required]
         public string Tasks { get; set; }
         public virtual Employee Employee { get; set; }
         public virtual Project Projects { get; set;}
