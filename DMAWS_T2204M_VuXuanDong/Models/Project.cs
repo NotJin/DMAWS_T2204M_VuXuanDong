d1b18ed5456f6d9858a0fa685cd7b35ea8e61ad1
Đồng Vũ <Dongvu7173@gmail.com>
2023-09-14 11:09:36 +0700

    4

--- DMAWS_T2204M_VuXuanDong/Models/Project.cs
+++ DMAWS_T2204M_VuXuanDong/Models/Project.cs
@@ -1,12 +1,24 @@
-﻿namespace DMAWS_T2204M_VuXuanDong.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DMAWS_T2204M_VuXuanDong.Models
 {
     public class Project
     {
+        [Key]
         public int ProjectId { get; set; }
+
+        [Required]
+        [StringLength(150, MinimumLength = 2)]
         public string ProjectName { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
         public DateTime ProjectStartDate { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime? ProjectEndDate { get; set; }
-        public virtual ICollection<ProjectEmployee> ProjectEmployees { get; set;}
+
+        public virtual ICollection<ProjectEmployee> ProjectEmployees { get; set; }
 
     }
 }
